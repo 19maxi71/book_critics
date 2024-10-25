0f82d5a84bbda9ffe2a7bf94b645fdd9b1f7e5b8
19maxi71 <131301678+19maxi71@users.noreply.github.com>
2024-10-26 01:06:20 +0200

    img problem résolu

--- blog/forms.py
+++ blog/forms.py
@@ -8,6 +8,14 @@ class TicketForm(forms.ModelForm):
         fields = ['title', 'description', 'image']
         
 class ReviewForm(forms.ModelForm):
+    RATING_CHOICES = [(i, str(i)) for i in range(6)]  # Choices from 0 to 5
+
+    rating = forms.ChoiceField(
+        choices=RATING_CHOICES,
+        widget=forms.RadioSelect,
+        label='Rating'
+    )
+
     class Meta:
         model = Review
         fields = ['rating', 'headline', 'body']
--- book_critics/settings.py
+++ book_critics/settings.py
@@ -131,3 +131,6 @@ LOGOUT_REDIRECT_URL = '/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
