4a22b66ea7f3ab2d23099d9f31beb3e7c9592efe
19maxi71 <131301678+19maxi71@users.noreply.github.com>
2024-10-30 19:13:39 +0100

    création rapport flake8

--- blog/forms.py
+++ blog/forms.py
@@ -1,29 +1,34 @@
 from django import forms
-from .models import Ticket, Review, UserFollows
-# from django.contrib.auth.models import User
+from .models import Review, UserFollows
 
-class TicketForm(forms.ModelForm):
-    class Meta:
-        model = Ticket
-        fields = ['title', 'description', 'image']
-        
 class ReviewForm(forms.ModelForm):
-    RATING_CHOICES = [(i, str(i)) for i in range(6)]  # Choices from 0 to 5
+    """
+    Formulaire pour créer ou éditer une critique.
+    """
+    RATING_CHOICES = [(i, str(i)) for i in range(6)]  # Choix de 0 à 5
 
     rating = forms.ChoiceField(
         choices=RATING_CHOICES,
         widget=forms.RadioSelect,
-        label='Rating'
+        label='Évaluation'
     )
 
     class Meta:
         model = Review
         fields = ['rating', 'headline', 'body']
 
+
 class FollowUserForm(forms.ModelForm):
+    """
+    Formulaire pour suivre un utilisateur.
+    """
     class Meta:
         model = UserFollows
         fields = ['followed_user']
 
+
 class UserSearchForm(forms.Form):
-    username = forms.CharField(max_length=150, required=False, label='Search for users')
+    """
+    Formulaire pour rechercher des utilisateurs.
+    """
+    username = forms.CharField(max_length=150, required=False, label='Rechercher des utilisateurs')
--- blog/models.py
+++ blog/models.py
@@ -2,17 +2,26 @@ from django.db import models
 from django.conf import settings
 from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here. On fait comme indiqué sur UML
+# Créez vos modèles ici. On fait comme indiqué sur UML
 
 class Ticket(models.Model):
+    """
+    Modèle représentant un ticket de demande de critique.
+    """
     title = models.CharField(max_length=128)
     description = models.TextField(max_length=2048, blank=True)
     user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    image = models.ImageField(null=True, blank=True)    
+    image = models.ImageField(null=True, blank=True)
     time_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
 
 
 class Review(models.Model):
+    """
+    Modèle représentant une critique pour un ticket.
+    """
     ticket = models.ForeignKey(to=Ticket, on_delete=models.CASCADE)
     rating = models.PositiveSmallIntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
     user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -20,18 +29,14 @@ class Review(models.Model):
     body = models.TextField(max_length=8192, blank=True)
     time_created = models.DateTimeField(auto_now_add=True)
 
-    # class Meta:
-    #     unique_together = ('user', 'ticket')
-    
-    # def clean(self):
-    #     if Review.objects.filter(ticket=self.ticket, user=self.user).exists():
-    #         raise ValidationError('You have already reviewed this ticket.')
+    def __str__(self):
+        return self.headline
 
-    # def save(self, *args, **kwargs):
-    #     self.clean()
-    #     super().save(*args, **kwargs)
 
 class UserFollows(models.Model):
+    """
+    Modèle représentant la relation de suivi entre les utilisateurs.
+    """
     user = models.ForeignKey(to=settings.AUTH_USER_MODEL, related_name='following', on_delete=models.CASCADE)
     followed_user = models.ForeignKey(to=settings.AUTH_USER_MODEL, related_name='followed_by', on_delete=models.CASCADE)
 
