896343cb1b2f48d15abac10608720fbe33cd9db1
19maxi71 <131301678+19maxi71@users.noreply.github.com>
2024-10-31 11:17:01 +0100

    django secret key hided

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.contrib import admin
 from .models import Ticket, Review, UserFollows
 
 # Register your models here.
--- book_critics/settings.py
+++ book_critics/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a_f*q)l-m-yh7m28hqs2iht4xzio)-%9*nozntj9tka-u$2=)+'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
